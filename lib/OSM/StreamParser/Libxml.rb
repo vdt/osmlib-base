97a6dc5d70a0fe5714e2d1dc50dc4722e330254d
Serge Wroclawski <serge@wroclawski.org>
2012-03-01 23:46:22 -0500

    Reformatting source code as per Emacs indent levels

--- lib/OSM/StreamParser/Libxml.rb
+++ lib/OSM/StreamParser/Libxml.rb
@@ -2,49 +2,51 @@
 
 require 'rubygems'
 begin
-    require 'xml/libxml'
+  require 'xml/libxml'
 rescue LoadError
-    require 'libxml'
+  require 'libxml'
 end
 
 # Namespace for modules and classes related to the OpenStreetMap project.
 module OSM
 
-    # Stream parser for OpenStreetMap XML files using Libxml.
-    class StreamParser::Libxml < StreamParserBase
+  # Stream parser for OpenStreetMap XML files using Libxml.
+  class StreamParser::Libxml < StreamParserBase
 
-        # Create new StreamParser object. Only argument is a hash.
-        #
-        # call-seq: OSM::StreamParser.new(:filename => 'filename.osm')
-        #           OSM::StreamParser.new(:string => '...')
-        #
-        # The hash keys:
-        #   :filename  => name of XML file
-        #   :string    => XML string
-        #   :db        => an OSM::Database object
-        #   :callbacks => an OSM::Callbacks object (or more likely from a derived class)
-        #                 if none was given a new OSM:Callbacks object is created
-        #
-        # You can only use :filename or :string, not both.
-        def initialize(options)
-            super(options)
+    # Create new StreamParser object. Only argument is a hash.
+    #
+    # call-seq: OSM::StreamParser.new(:filename => 'filename.osm')
+    #           OSM::StreamParser.new(:string => '...')
+    #
+    # The hash keys:
+    #   :filename  => name of XML file
+    #   :string    => XML string
+    #   :db        => an OSM::Database object
+    #   :callbacks => an OSM::Callbacks object (or more likely from a
+    #   derived class)
+    #                 if none was given a new OSM:Callbacks object is
+    #                 created
+    #
+    # You can only use :filename or :string, not both.
+    def initialize(options)
+      super(options)
 
-            @parser = XML::SaxParser.new
-            if @filename.nil?
-                @parser.string = @string
-            else
-                @parser.filename = @filename
-            end
-            @parser.callbacks = @callbacks
-        end
-
-        # Run the parser. Return value is the return value of the OSM::Callbacks#result method.
-        def parse
-            @parser.parse
-            @callbacks.result
-        end
-
+      @parser = XML::SaxParser.new
+      if @filename.nil?
+        @parser.string = @string
+      else
+        @parser.filename = @filename
+      end
+      @parser.callbacks = @callbacks
     end
 
-end
+    # Run the parser. Return value is the return value of the
+    # OSM::Callbacks#result method.
+    def parse
+      @parser.parse
+      @callbacks.result
+    end
 
+  end
+
+end
